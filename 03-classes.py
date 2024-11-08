ce479d9586ad8e57fa2f774a0166d4dfd7fe8ecc
Adrian Scherrer <adrian.scherrer@metrohm.com>
2024-11-08 19:17:28 +0100

    Additional examples

--- 03-classes.py
+++ 03-classes.py
@@ -1,6 +1,7 @@
 from datetime import date
 from typing import Self
 
+
 # __xyz__ Methods are called dunder (double underscore) methods.
 # Here's an example to initialize (__init__) or represent (__repr__) an object
 
@@ -19,6 +20,7 @@ class Person:
     def age_diff(self, other_person: Self) -> int:
         return abs((self.birthday - other_person.birthday).days)
 
+
 adi: Person = Person('Adi', date(year=1970, month=3, day=3), 'adi.scherrer@gmail.com')
 yvonne: Person = Person('Yvonne', date(year=1970, month=4, day=13), 'adi.scherrer@gmail.com')
 print(adi)
@@ -26,9 +28,31 @@ print(yvonne)
 print(f"{adi.name}'s age is {adi.age_in_days()} days")
 print(f"{yvonne.name}'s age is {yvonne.age_in_days()} days")
 print(f"Age difference between {yvonne.name} and {adi.name} is {adi.age_diff(yvonne)} days")
+print("\n--------------------------------------------------------------------------------")
 
 # Class info
 print(f'{dir(Person)=}')
 print(f'{adi.__dict__=}')
 print(f'{adi.__dict__.keys()=}')
 print(f'{adi.__dict__.values()=}')
+print("\n--------------------------------------------------------------------------------")
+
+
+# -----------------------------------------------------------------------------
+
+# Returning "self" will allow method chaining
+class Calc:
+    def __init__(self, value: int = 0):
+        self.value = value
+
+    def increment(self, amount: int):
+        self.value += amount
+        return self  # Return self to allow chaining
+
+    def multiply(self, factor: int):
+        self.value *= factor
+        return self  # Return self to allow chaining
+
+
+print(f'{Calc(0).increment(5).multiply(3).value=}')
+print(f'{Calc(-2).multiply(3).increment(12).multiply(6).value=}')
