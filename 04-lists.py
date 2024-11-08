d39d963062e0714a7af28a256075db8e2539ff4f
Adrian Scherrer <adrian.scherrer@metrohm.com>
2024-11-08 19:50:33 +0100

    Additional examples

--- 04-lists.py
+++ 04-lists.py
@@ -1,11 +1,15 @@
 names: list[str] = ['Luigi', 'Yvonne', 'Adi', 'Thomas', 'Gulia', 'Bob']
 numbers: list[int] = [95, 74, 82, 15, 14, 3, 25, 4, 64, 75, 32, 12]
 
-print('Normal order:   ', ', '.join(names))
-print('Reversed order: ', ', '.join(names[::-1]))
+print(f'Normal list style:            {names}')
+print('Print names nicely formatted: ', end='')
+print(*names, sep=' - ', end='.\n')
+print('Normal order:                ', ', '.join(names))
+print('Reversed order:              ', ', '.join(names[::-1]))
 print("\n--------------------------------------------------------------------------------")
 
-# Sorted numbers greater than 40
+print(f'{numbers=}')
+print('Sorted numbers greater than 40')
 print([num for num in sorted(numbers) if num > 40])
 print("\n--------------------------------------------------------------------------------")
 
